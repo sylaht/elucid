19991d505d770a906ec65671f97d4aa793ff3db8
Thalys001 <thalyssouza708@gmail.com>
2025-10-11 11:50:16 -0300

    feat(cli): add command handler with basic commands

---
+++ src/commands.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <string.h>
+#include "commands.h"
+
+void handle_command(int argc, char *argv[])
+{
+  if (argc < 2)
+  {
+    printf("Elucid - CLI for notes\n");
+    printf("Usage: elucid <command>\n");
+    printf("Commands available: new, list, view, edit, delete\n");
+    return;
+  }
+
+  const char *command = argv[1];
+
+  if (strcmp(command, "new") == 0)
+  {
+    printf("Creating new note...\n");
+  }
+  else if (strcmp(command, "list") == 0)
+  {
+    printf("Listing notes...\n");
+  }
+  else if (strcmp(command, "view") == 0)
+  {
+    printf("Viewing note...\n");
+  }
+  else if (strcmp(command, "edit") == 0)
+  {
+    printf("Editing note...\n");
+  }
+  else if (strcmp(command, "delete") == 0)
+  {
+    printf("Deleting note...\n");
+  }
+  else if (strcmp(command, "help") == 0)
+  {
+    printf("Commands available: new, list, view, edit, delete\n");
+  }
+  else
+  {
+    printf("Unknown command: %s\n", command);
+    printf("Use 'elucid help' to see the available commands.\n");
+  }
+}
